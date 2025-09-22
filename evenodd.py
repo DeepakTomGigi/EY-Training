3f5415d6ccc0de1e06b6b065b0c919482a70b31c
Deepak <kannanbrodtg@gmail.com>
2025-09-22 11:36:49 +0530

    sep 22

---
+++ evenodd.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry"]
+
+print(fruits)
+
+fruits.append("orange")
+print(fruits)
+
+fruits.insert(0, "mango")
+
+print(fruits)
+
+fruits.remove("orange")
+print(fruits)
+fruits.pop()
+print(fruits)
